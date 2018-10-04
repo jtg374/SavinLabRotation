eede4b18039da39b66cc597936d8e39aedadd0ec
jtg374 <gtolden@gmail.com>
2018-10-03 22:10:27 -0400

    add NDF single unit

---
+++ NDF_singleEI_nonlinear.py
@@ -0,0 +1,40 @@
+#%%
+import numpy
+from matplotlib import pyplot
+from scipy.integrate import odeint
+sig = lambda x: 1/(1+numpy.exp(-x))
+
+#%% parameter
+tEE = 100
+tEI = 10
+tIE = 25
+tII = 10
+tO = 5
+JEE = 100
+JEI = 100
+JIE = 100
+JII = 100
+JO  = 50
+stim = lambda t:int(t==1)
+# main ode
+def NDF(y,t):
+    sEE,sEI,sIE,sII,iE = y
+    E = sig(JEE*sEE-JEI*sEI+iE) 
+    I = sig(JIE*sIE-JII*sII)
+    dEE = (-sEE + E)/tEE
+    dIE = (-sIE + E)/tIE
+    dEI = (-sEI + I)/tEI
+    dII = (-sII + I)/tII
+    diE = (-iE  + JO*stim(t))/tO
+    return [dEE,dEI,dIE,dII,diE]
+#%%
+y0 = [.5,.5,.5,.5,0]
+t = numpy.arange(10000)
+y = odeint(NDF,y0,t)
+
+#%%
+iE = y[:,4]
+pyplot.plot(iE)
+#%%
+sEE = y[:,0]
+pyplot.plot(sEE)
