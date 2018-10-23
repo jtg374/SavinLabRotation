f36e204ff45ca9e302520dbfeb824066eeb5df91
jtg374 <gtolden@gmail.com>
2018-10-23 18:08:48 -0400

    add new way to stimulate

--- NDF_singleEI_nonlinear.py
+++ NDF_singleEI_nonlinear.py
@@ -28,10 +28,19 @@ def NDF(t,y):
     dII = (-sII + I)/tII
     diE = (-iE  + JO*stim(t))/tO
     return [dEE,dEI,dIE,dII,diE]
-#%%
+#%% stim by external
 y0 = [.5,.5,.5,.5,0]
 t = numpy.arange(1000)
 for JO in [-200,200,2e3,2e4,2e5,2e9,2e12]:
+    y = odeint(NDF,y0,t,tfirst=True)
+    sEE,sEI,sIE,sII,iE = y[:,0],y[:,1],y[:,2],y[:,3],y[:,4]
+    E = sig(JEE*sEE-JEI*sEI+iE) 
+    pyplot.plot(E)
+#%% stim by initial value
+JO = 0
+t = numpy.arange(1000)
+for E0 in [-0.005,0.005,0.01,0.02,0.04]:
+    y0 = [E0+0.5,E0+0.5,.5,.5,0]
     y = odeint(NDF,y0,t,tfirst=True)
     sEE,sEI,sIE,sII,iE = y[:,0],y[:,1],y[:,2],y[:,3],y[:,4]
     E = sig(JEE*sEE-JEI*sEI+iE) 
