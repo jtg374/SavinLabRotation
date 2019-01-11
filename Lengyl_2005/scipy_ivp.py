2ae9a3f79ad8370ddce4e6159502da498939f996
jtg374Pro6 <jtg374@nyu.edu>
2019-01-11 14:00:49 -0500

    modify solver

---
+++ Lengyl_2005/scipy_ivp.py
@@ -0,0 +1,292 @@
+import inspect
+import numpy as np
+from scipy.optimize import OptimizeResult
+from scipy.integrate import OdeSolver,OdeSolution
+from scipy.integrate import RK45
+EPS = np.finfo(float).eps
+
+## Below copied from scipy.integrate
+
+MESSAGES = {0: "The solver successfully reached the end of the integration interval.",
+            1: "A termination event occurred."}
+
+class OdeResult(OptimizeResult):
+    pass
+
+
+def prepare_events(events):
+    """Standardize event functions and extract is_terminal and direction."""
+    if callable(events):
+        events = (events,)
+
+    if events is not None:
+        is_terminal = np.empty(len(events), dtype=bool)
+        direction = np.empty(len(events))
+        for i, event in enumerate(events):
+            try:
+                is_terminal[i] = event.terminal
+            except AttributeError:
+                is_terminal[i] = False
+
+            try:
+                direction[i] = event.direction
+            except AttributeError:
+                direction[i] = 0
+    else:
+        is_terminal = None
+        direction = None
+
+    return events, is_terminal, direction
+
+
+def solve_event_equation(event, sol, t_old, t):
+    """Solve an equation corresponding to an ODE event.
+
+    The equation is ``event(t, y(t)) = 0``, here ``y(t)`` is known from an
+    ODE solver using some sort of interpolation. It is solved by
+    `scipy.optimize.brentq` with xtol=atol=4*EPS.
+
+    Parameters
+    ----------
+    event : callable
+        Function ``event(t, y)``.
+    sol : callable
+        Function ``sol(t)`` which evaluates an ODE solution between `t_old`
+        and  `t`.
+    t_old, t : float
+        Previous and new values of time. They will be used as a bracketing
+        interval.
+
+    Returns
+    -------
+    root : float
+        Found solution.
+    """
+    from scipy.optimize import brentq
+    return brentq(lambda t: event(t, sol(t)), t_old, t,
+                  xtol=4 * EPS, rtol=4 * EPS)
+
+
+def handle_events(sol, events, active_events, is_terminal, t_old, t):
+    """Helper function to handle events.
+
+    Parameters
+    ----------
+    sol : DenseOutput
+        Function ``sol(t)`` which evaluates an ODE solution between `t_old`
+        and  `t`.
+    events : list of callables, length n_events
+        Event functions with signatures ``event(t, y)``.
+    active_events : ndarray
+        Indices of events which occurred.
+    is_terminal : ndarray, shape (n_events,)
+        Which events are terminal.
+    t_old, t : float
+        Previous and new values of time.
+
+    Returns
+    -------
+    root_indices : ndarray
+        Indices of events which take zero between `t_old` and `t` and before
+        a possible termination.
+    roots : ndarray
+        Values of t at which events occurred.
+    terminate : bool
+        Whether a terminal event occurred.
+    """
+    roots = []
+    for event_index in active_events:
+        roots.append(solve_event_equation(events[event_index], sol, t_old, t))
+
+    roots = np.asarray(roots)
+
+    if np.any(is_terminal[active_events]):
+        if t > t_old:
+            order = np.argsort(roots)
+        else:
+            order = np.argsort(-roots)
+        active_events = active_events[order]
+        roots = roots[order]
+        t = np.nonzero(is_terminal[active_events])[0][0]
+        active_events = active_events[:t + 1]
+        roots = roots[:t + 1]
+        terminate = True
+    else:
+        terminate = False
+
+    return active_events, roots, terminate
+
+
+def find_active_events(g, g_new, direction):
+    """Find which event occurred during an integration step.
+
+    Parameters
+    ----------
+    g, g_new : array_like, shape (n_events,)
+        Values of event functions at a current and next points.
+    direction : ndarray, shape (n_events,)
+        Event "direction" according to the definition in `solve_ivp`.
+
+    Returns
+    -------
+    active_events : ndarray
+        Indices of events which occurred during the step.
+    """
+    g, g_new = np.asarray(g), np.asarray(g_new)
+    up = (g <= 0) & (g_new >= 0)
+    down = (g >= 0) & (g_new <= 0)
+    either = up | down
+    mask = (up & (direction > 0) |
+            down & (direction < 0) |
+            either & (direction == 0))
+
+    return np.nonzero(mask)[0]
+
+# above copied from scipy.integrate #
+# below copied from scipy.integrate # 
+
+def solve_with_event(fun, t_span,y0, method=RK45, t_eval=None,
+    dense_output=False, events=None,vectorized=False, **options):
+    # below copied from scipy.integrate # Prepare output
+    t0, tf = float(t_span[0]), float(t_span[1])
+
+    if t_eval is not None:
+        t_eval = np.asarray(t_eval)
+        if t_eval.ndim != 1:
+            raise ValueError("`t_eval` must be 1-dimensional.")
+
+        if np.any(t_eval < min(t0, tf)) or np.any(t_eval > max(t0, tf)):
+            raise ValueError("Values in `t_eval` are not within `t_span`.")
+
+        d = np.diff(t_eval)
+        if tf > t0 and np.any(d <= 0) or tf < t0 and np.any(d >= 0):
+            raise ValueError("Values in `t_eval` are not properly sorted.")
+
+        if tf > t0:
+            t_eval_i = 0
+        else:
+            # Make order of t_eval decreasing to use np.searchsorted.
+            t_eval = t_eval[::-1]
+            # This will be an upper bound for slices.
+            t_eval_i = t_eval.shape[0]
+
+    # above copied from scipy.integrate #
+
+    # below copied from scipy.integrate # Prepare output
+    if t_eval is None:
+        ts = [t0]
+        ys = [y0]
+    else:
+        ts = []
+        ys = []
+
+    interpolants = []
+
+    events, is_terminal, event_dir = prepare_events(events)
+
+    if events is not None:
+        g = [event(t0, y0) for event in events]
+        t_events = [[] for _ in range(len(events))]
+    # above copied from scipy.integrate #
+        # add line
+        t_events_last = [np.NaN for _ in range(len(events))]
+    # below copied from scipy.integrate # Prepare output
+    else:
+        t_events = None
+    # above copied from scipy.integrate #
+    
+    funWithEvent = lambda t,y: fun(t,y,t_events_last)
+    solver = RK45(funWithEvent, t0, y0, tf, vectorized=vectorized, **options)
+
+    # below copied from scipy.integrate # Start integration
+    status = None
+    while status is None:
+        message = solver.step()
+
+        if solver.status == 'finished':
+            status = 0
+        elif solver.status == 'failed':
+            status = -1
+            break
+
+        t_old = solver.t_old
+        t = solver.t
+        y = solver.y
+
+        if dense_output:
+            sol = solver.dense_output()
+            interpolants.append(sol)
+        else:
+            sol = None
+
+        if events is not None:
+            g_new = [event(t, y) for event in events]
+            active_events = find_active_events(g, g_new, event_dir)
+            if active_events.size > 0:
+                if sol is None:
+                    sol = solver.dense_output()
+
+                root_indices, roots, terminate = handle_events(
+                    sol, events, active_events, is_terminal, t_old, t)
+
+                for e, te in zip(root_indices, roots):
+                    t_events[e].append(te)
+    # above copied from scipy.integrate #
+                    # add lines
+                    t_events_last[e]= te
+                    funWithEvent = lambda t,y: fun(t,y,t_events_last)
+                    solver = RK45(funWithEvent, t, y, tf, vectorized=vectorized, **options)
+    # below copied from scipy.integrate #
+
+                if terminate:
+                    status = 1
+                    t = roots[-1]
+                    y = sol(t)
+
+            g = g_new
+
+        if t_eval is None:
+            ts.append(t)
+            ys.append(y)
+        else:
+            # The value in t_eval equal to t will be included.
+            if solver.direction > 0:
+                t_eval_i_new = np.searchsorted(t_eval, t, side='right')
+                t_eval_step = t_eval[t_eval_i:t_eval_i_new]
+            else:
+                t_eval_i_new = np.searchsorted(t_eval, t, side='left')
+                # It has to be done with two slice operations, because
+                # you can't slice to 0-th element inclusive using backward
+                # slicing.
+                t_eval_step = t_eval[t_eval_i_new:t_eval_i][::-1]
+
+            if t_eval_step.size > 0:
+                if sol is None:
+                    sol = solver.dense_output()
+                ts.append(t_eval_step)
+                ys.append(sol(t_eval_step))
+                t_eval_i = t_eval_i_new
+
+    message = MESSAGES.get(status, message)
+
+    if t_events is not None:
+        t_events = [np.asarray(te) for te in t_events]
+
+    if t_eval is None:
+        ts = np.array(ts)
+        ys = np.vstack(ys).T
+    else:
+        ts = np.hstack(ts)
+        ys = np.hstack(ys)
+
+    if dense_output:
+        sol = OdeSolution(ts, interpolants)
+    else:
+        sol = None
+
+    return OdeResult(t=ts, y=ys, sol=sol, t_events=t_events, nfev=solver.nfev,
+                     njev=solver.njev, nlu=solver.nlu, status=status,
+                     message=message, success=status >= 0)
+
+    # above copied from scipy.integrate #
+
