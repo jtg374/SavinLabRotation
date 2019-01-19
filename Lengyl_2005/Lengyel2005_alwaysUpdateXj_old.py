57606d055ac951a469add1222401aacd09c5af90
jtg374Pro6 <jtg374@nyu.edu>
2019-01-19 10:22:30 -0500

    for PRC curve, k_prior has a large effect independent of T_theta.

---
+++ Lengyl_2005/Lengyel2005_alwaysUpdateXj_old.py
@@ -0,0 +1,145 @@
+#%%
+import numpy as np
+from numpy import pi,exp,sin,cos
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+#%%
+## Define STDP and Phase coupling function
+A_STDP = 0.03
+s_STDP = 4
+T_theta = 125 # theta oscillation period in ms
+dp = lambda dt: dt*2*pi/T_theta # dt = xi - xj
+omega = lambda dx: A_STDP * exp(s_STDP*cos(dx)) * sin(dx)
+# derivative in respect to xi
+domega = lambda dx: 2*pi/T_theta*A_STDP*exp(s_STDP*cos(dx)) * (cos(dx) - s_STDP * sin(dx)**2 )
+
+#%%
+du=np.pi/120 
+u = np.arange(-np.pi,np.pi,du) # a bunch of testing phase 
+uu=u.copy();uu[u<=0]+=2*np.pi # wrap into range (0,2pi]
+PRC = np.zeros_like(u)
+k_prior = 0.6 # confidence of prior (x=0)
+for w in [0.01,0.025, 0.05, 0.075]:
+        for i,xj in enumerate(uu):
+                x=0 # postsynaptic start from 0 phase
+                t=xj # phase response integrate from presynaptic fire
+                dt = 0.001*T_theta
+                while (dp(t+dt)-x)<2*np.pi: # fire when x == t mod 2pi
+                        dx = dt*(-k_prior*np.sin(x)+w*domega(x-xj))
+                        x+=dx
+                        t+=dt
+                PRC[i] = x
+        plt.plot(u,PRC)
+#%%
+## Create Memorys
+N = 200 # number of neurons
+M = 10 # number of memorys
+k_prior = 0.5 # concentration parameter for prior distribution
+k_cue = 10 # for cue distribution
+xMemory = np.random.vonmises(0,k_prior,(N,M))
+## Create Synapses
+W = np.zeros((N,N))
+for i in range(N):
+    for j in range(i): # 0<=j<i
+        for k in range(M):
+            W[i,j] += omega(xMemory[i,k]-xMemory[j,k])
+            W[j,i] += omega(xMemory[j,k]-xMemory[i,k])
+sigma2_W = np.var(W.flatten())
+
+#%%
+## Define ODE
+def mainode(t,x,N,W,sigma2_W,x_tilde,k_prior,k_cue):
+    # Additional parameters
+    # N: #neurons
+    # W: Synpatic weight W[i,j] is from j to i
+    # sigma2_W: variance of W
+    # x_tilde is the recall cue
+    # Calculate phase response H
+    H = np.zeros(N)
+    for i in range(N):
+        dxi = x[i] - x # dxi[j] = x[i] - x[j]
+        H[i] = np.dot( W[i,:], domega(dxi) ) # H[i] = \sum_j W_{ij} * domega(xi-xj)
+    #
+    dx_prior    = -k_prior * sin(x)
+    dx_external = -k_cue * sin(x-x_tilde)
+    dx_synapse  = H/sigma2_W
+    dx = dx_prior + dx_external + dx_synapse
+    return dx
+#%%
+## Solve ODE
+# Initial Condintion
+k = 0 # memory to recall
+xTarget = xMemory[:,k]
+x0 = xTarget.copy() # np.random.vonmises(0,k_prior,N)
+xNoise = np.random.vonmises(0,k_cue,N)
+x_tilde = xTarget + xNoise
+
+# Define firing events
+events = [lambda t,x,j=j: sin((x[j] - 2*pi*t/T_theta)/2) for j in range(N)]
+# events[i] = 0 if and only if x[i] == 2*pi*t/T mod 2pi
+
+# Integration
+tf = T_theta
+kwargs = {
+    'N': N,
+    'W': W,
+    'k_prior': k_prior,
+    'k_cue': k_cue,
+    'sigma2_W': sigma2_W,
+    'x_tilde': x_tilde
+}
+sol = solve_ivp(lambda t,y: mainode(t,y,**kwargs),(0,tf),x0,events=events)
+t   = sol.t; tNow = sol.t[-1]
+x_t = sol.y; xNow = sol.y[:,-1]
+t_fire = sol.t_events
+x_fire = [np.mod(ts/T_theta,1)*2*pi for ts in t_fire]
+print(sol.message)
+
+#%%
+# show time course
+from matplotlib.cm import get_cmap
+hsv = get_cmap('hsv')
+ax = plt.subplot(111,projection='polar')
+for xi_t,target in zip(x_t,xTarget):
+    color = hsv((target/pi/2)%1)
+    ax.plot(xi_t,t,color=color,alpha=0.2)
+ax.plot(2*pi*t/T_theta,t,color='white')
+ax.set_ylabel('time')
+
+#%%
+## evaluate errors
+errors = x_t - np.transpose(np.tile(xTarget,(len(t),1)))
+errors = np.mod(errors+pi,2*pi)-pi
+h=plt.hist(errors[:,-1])
+plt.xlim((-pi,pi))
+plt.xlabel('error')
+plt.ylabel('counts')
+
+#%%
+# show time course of error
+ax = plt.subplot(projection='polar')
+for dxi_t,target in zip(errors,xTarget):
+    color = hsv((target/pi/2)%1)
+    ax.plot(dxi_t,t,color=color,alpha=0.1)
+ax.set_ylabel('time')
+ax.set_rlabel_position(135)
+
+#%%
+# Continue Integration
+tf += 10*T_theta
+sol = solve_ivp(lambda t,y: mainode(t,y,**kwargs),(tNow,tf),xNow,events=events,t_eval=np.arange(tNow,tf,5))
+t   = sol.t; tNow = sol.t[-1]
+x_t = sol.y; xNow = sol.y[:,-1]
+t_fire = sol.t_events
+x_fire = [np.mod(ts/T_theta,1)*2*pi for ts in t_fire]
+print(sol.message)
+
+## evaluate errors
+errors = xNow - xTarget
+errors = np.mod(errors+pi,2*pi)-pi
+h=plt.hist(errors)
+plt.xlim((-pi,pi))
+plt.xlabel('error')
+plt.ylabel('counts')
+
+#%%
